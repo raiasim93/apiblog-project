65dda0c65e90c88bd8f3c7773700586b93f9c279
raiasim93 <asimrai89@gmail.com>
2024-01-26 00:01:04 +1100

    starting structure for both client and api server

---
+++ client.js
@@ -0,0 +1,17 @@
+// importing modules again
+import express from "express";
+import bodyParser from "body-parser";
+import axios from "axios";
+
+// assiginig a different port
+const app = express();
+const port = 3000;
+const API_URL = "http:/localhost:4000/"
+
+// using middleware here
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));
+
+app.listen(port, ()=>{
+  console.log(`Server running at port: ${port}`);
+});
