b4def3d761cb6ebf854eb3ca3ac76c251a1f64d5
raiasim93 <asimrai89@gmail.com>
2024-01-26 13:11:38 +1100

    work on both servers, ensured they are functioning, need to fix the update button only

--- client.js
+++ client.js
@@ -6,14 +6,93 @@ import axios from "axios";
 // assiginig a different port
 const app = express();
 const port = 3000;
-const API_URL = "http:/localhost:4000/"
+const API_URL = "http://localhost:4000"
 
 // using middleware here
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(express.static("public"));
 
 
 // hitting the home route
+
+app.get("/", async (req, res)=>{
+  try {
+    const response = await axios.get(`${API_URL}/posts`);
+    res.render("index.ejs",{ 
+      posts: response.data
+    });
+  } 
+  catch (error) {
+    res.status(500).json({message: `Error Retrieving Posts`});
+  }
+});
+
+// route to render to the new page
+app.get("/new", (req, res)=> {
+  res.render("modify.ejs", {
+    heading: "New Post",
+    submit: "Create post"
+  })
+})
+
+app.get("/edit/:id", async (req, res)=> {
+  try {
+    const response = await axios.get(`${API_URL}/posts/${req.params.id}`);
+    res.render("modify.ejs",{
+      heading: "Edit Post",
+      submit: "Update Post",
+      post: response.data,
+    });
+  } 
+  catch (error) {
+    res.status(500).json({message: `Error fetching post!`});
+  }
+});
+
+// creating a new post 
+app.post("/api/posts", async(req, res)=> {
+  try {
+    const response = await axios.post(`${API_URL}/posts`, req.body);
+    console.log(response.data);
+    res.redirect("/");  
+  } 
+  catch (error) {
+    res.status(500).json({message: `Error Creating a Post`});
+  }
+});
+
+app.post("/api/posts/:id", async(req, res)=> {
+  try {
+    const response = await axios.patch(
+      `${API_URL}/posts/${req.params/id}`, req.body
+    );
+    console.log(response.data);
+    res.redirect("/")
+  } 
+  catch (error) {
+    res.status(500).json({message: `Error updating the post`});
+  };
+});
+
+app.get("/api/posts/delete/:id", async(req, res)=>{
+  try {
+    await axios.delete(
+      `${API_URL}/posts/${req.params.id}`
+    );
+    res.redirect("/");  
+  } 
+  catch (error) {
+    res.status(500).json({message: `Error deleting the post`});
+  };
+});
+
+
+
+
+
+
+
 app.listen(port, ()=>{
   console.log(`Server running at port: ${port}`);
 });
--- index.js
+++ index.js
@@ -34,7 +34,7 @@ let posts = [
     date: "2023-08-10T09:15:00Z",
   },
 ];
-const lastId = 3;
+let lastId = 3;
 
 //using middlewares
 app.use(bodyParser.urlencoded({extended: true}));
@@ -44,25 +44,37 @@ app.use(bodyParser.json());
 
 // get route for all jokes 
 
-app.get("/jokes",(req, res)=> {
-  res.json(jokes);
+app.get("/posts",(req, res)=> {
+  console.log(posts);
+  res.json(posts);
 })
 
-app.get("/jokes/:id", (req, res)=> {
+app.get("/posts/:id", (req, res)=> {
   const post = posts.find((p)=> p.id === parseInt(req.params.id));
   if(!post) return res.status(404).json({message: `Joke not found `});
   res.json(post);
 })
 
 app.post("/posts", (req, res)=> {
-  const newId = lastId + 1;
-  const post = {
-    id : newId,
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author,
-    date: new Date()
-  };
+  // let  newId = lastId + 1;
+  // const post = {
+  //   id : newId,
+  //   title: req.body.title,
+  //   content: req.body.content,
+  //   author: req.body.author,
+  //   date: new Date()
+  // };
+  // lastId = newId;
+  // posts.push(post);
+  // res.status(201).json(post);
+      const newId = lastId += 1;
+      const post = {
+        id: newId,
+        title: req.body.title,
+        content: req.body.content,
+        author: req.body.author,
+        date: new Date(),
+      };
   lastId = newId;
   posts.push(post);
   res.status(201).json(post);
@@ -82,12 +94,12 @@ app.patch("/posts/:id", (req, res)=>{
 
 
 app.delete("/posts/:id", (req, res)=> {
-const index = posts.findIndex((p)=> p.id === id);
-if(index < -1) res.status(404) .json({message})
+const index = posts.findIndex((p)=> p.id === parseInt(req.params.id));
+if(index === -1) res.status(404) .json({message});
+posts.splice(index, 1);
+res.json({message: `Joke Deleted `})
 });
 
-posts.splice(inded, 1);
-res.json({message: `Joke Deleted `})
 
 app.listen(port, ()=> {
   console.log(`Server running at port number: ${port}`);
